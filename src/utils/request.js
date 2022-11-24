4c4f1ac03175e0852cc470411394a89b36650f5d
lrqtt <lurui774862045@163.com>
2022-11-24 10:05:17 +0800

    人力资源项目

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,27 @@
 import axios from 'axios'
-const service = axios.create()
+import { Message } from 'element-ui'
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000 // 设置超时时间
+})
 service.interceptors.request.use()
-service.interceptors.response.use()
+// 响应拦截器
+service.interceptors.response.use(
+  response => {
+    // axios默认加了一层data
+    const { success, message, data } = response.data
+    // 要根据success的成功与否决定下面的操作
+    if (success) {
+      return data
+    } else {
+      Message.error(message) // 提示错误消息
+      return Promise.reject(new Error(message))
+    }
+  },
+  error => {
+    Message.error(error.message) // 提示错误信息
+    return Promise.reject(error) // 返回执行错误，让当前的执行链跳出成功，直接进入catch
+  }
+)
 
 export default service
